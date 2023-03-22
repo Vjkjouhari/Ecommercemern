c53c7c5adad74a4eb6e22bfe2262e73943441f95
Vjkjouhari <vejouhari@gmail.com>
2023-03-22 11:40:07 -0700

    added frontend setup

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/Ecommerce", {useNewUrlParser:true, useUnifiedTopology:true}).then(()=>{
+mongoose.connect("mongodb://localhost:27017/MernNew", {useNewUrlParser:true, useUnifiedTopology:true}).then(()=>{
         console.log("Connected to mongodb ");
     }).catch((err)=>{
         console.log(err);
